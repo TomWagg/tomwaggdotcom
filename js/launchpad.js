b17217211237d96aa57755e554033c8063a65e36
Tom Wagg <twagg@college.harvard.edu>
2021-03-13 16:30:52 -0500

    trim old code, add comments

--- js/launchpad.js
+++ js/launchpad.js
@@ -1,18 +1,9 @@
-console.log("hi");
-
-document.querySelectorAll(".research-projects .card").forEach(function (el) {
-    console.log(el);
-    el.addEventListener("click", function () {
-        this.parentElement.classList.toggle("col-3");
-        this.parentElement.classList.toggle("col-9");
-        this.querySelector(".hidden-text").classList.toggle("hide");
-    });
-});
-
+// flip the profile image every time the mouse 'flicks' it
 document.querySelector("img.profile").addEventListener("mouseout", function (e) {
     this.classList.toggle("flip");
 });
 
+// make projects bounce in, in sequence
 let i = 0;
 document.querySelectorAll(".project").forEach(function (el) {
     el.addEventListener("mouseenter", function() {
